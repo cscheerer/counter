bd4190f7452c7be63a61a4d2abb42460aceada8d
cscheerer <82324733+cscheerer@users.noreply.github.com>
2023-06-23 10:18:49 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# Simple Counter
+# by cscheerer on GitHub
+# Put in "reset" to reset the counter.
+import time
+
+count = 0
+newcount = 0
+
+while True:
+    time.sleep(0.2)
+    newcount = input("Add: ")
+
+    if newcount == "reset":
+        count = 0
+    else:
+        newcount = int(newcount)
+        count = count + newcount
+        print(count)
+
+
